b7f93351e91b0be9f2d87e1e889fe934a4442af3
EleonoraMercado <mercadoeleonora91@gmail.com>
2024-03-17 15:29:47 +0100

    ADD: inizio struttura  primo esercizio.

---
+++ js-snack1/js/app.js
@@ -0,0 +1,9 @@
+//L’utente inserisce due numeri in successione, con due prompt.
+let numeroA = parseFloat(prompt ("scegli un numero:"));
+
+let numeroB = parseFloat(prompt("Scegli un altro numero:"));
+
+//Il software stampa il maggiore.
+
+
+ 
