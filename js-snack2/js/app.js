12efc4e476865ed5b21eeed72bb97bf5c852ed68
EleonoraMercado <mercadoeleonora91@gmail.com>
2024-03-17 16:14:06 +0100

    ADD: impostazione della struttura.

---
+++ js-snack2/js/app.js
@@ -0,0 +1,19 @@
+//L’utente inserisce due parole in successione, con due prompt.
+
+let parola1 = prompt('Inserisci una parola che ti piace:'); //string 
+let parola2 = prompt('Inserisci un altra parola'); //string
+
+//Il software stampa prima la parola più corta, poi la parola più lunga.
+
+let corta, lunga; //string
+
+//stampare parola1 se piu lunga della parola2
+
+
+//stampare parola2 se è piu lunga della parola1
+
+
+//in caso l'utente inserisca parola della stessa lunghezza
+
+//stampare parola piu corta prima 
+
