f4acec9981a6100a904e3a73c1974d6ea9c7b5ee
EleonoraMercado <mercadoeleonora91@gmail.com>
2024-03-17 16:50:24 +0100

    ADD: struttura completa, fine.

--- js-snack2/js/app.js
+++ js-snack2/js/app.js
@@ -7,13 +7,26 @@ let parola2 = prompt('Inserisci un altra parola'); //string
 
 let corta, lunga; //string
 
-//stampare parola1 se piu lunga della parola2
+//stampare parola1 se piu corta della parola2
+if (parola1.length < parola2.length) {
+    lunga = parola2;
+    corta = parola1;
+}
 
-
-//stampare parola2 se è piu lunga della parola1
-
+//stampare parola2 se è piu corta della parola
+else if (parola2.length < parola1.length) {
+    lunga = parola1;
+    corta = parola2;
+}
 
 //in caso l'utente inserisca parola della stessa lunghezza
+else {
+    console.log('Le parole hanno la stessa lunghezza.') //string
+}
 
 //stampare parola piu corta prima 
+
+console.log('Parola corta:' + corta); //string
+console.log('parola lunga:' + lunga ); //string
+
 
