3b22fc075c425b82588737e10808ad8543976c73
EleonoraMercado <mercadoeleonora91@gmail.com>
2024-03-20 23:28:59 +0100

    ADD: inizio esercizio.

---
+++ js-snack7/js/app.js
@@ -0,0 +1,13 @@
+//Scrivere un programma che stampi la tabellina del 2, fino al numero 1000.
+//Modificate poi il programma in modo che venga chiesto all'utente il numero massimo consentito, 
+//e stampare tutta la tabellina del 2 fino al numero inserito.
+
+//chiedo all'utente di scegliere un numero massimo
+cost numeroMassimo = parseFloat(prompt('Inserisci un numero massimo'));
+
+let tabellina = '';
+
+for (let i = 1; i < 1000; i++) {
+   
+
+}
